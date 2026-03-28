a693b9ed35b1b792d9415a3bf464266106614897
yaiza42-cmd <mcuentapersonal42@gmail.com>
2026-03-28 10:12:13 -0400

    Update twrp_austin.mk

--- twrp_austin.mk
+++ twrp_austin.mk
@@ -1,7 +1,8 @@
 # Heredar de la base de AOSP para TWRP
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Identificadores del dispositivo (Muy importantes para el constructor)
+# Identificadores del dispositivo
 PRODUCT_DEVICE := austin
 PRODUCT_NAME := twrp_austin
 PRODUCT_BRAND := motorola
@@ -10,3 +11,6 @@ PRODUCT_MANUFACTURER := motorola
 
 # Configuración de la compilación
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+# Forzar arquitectura 64 bits
+TARGET_IS_64_BIT := true
