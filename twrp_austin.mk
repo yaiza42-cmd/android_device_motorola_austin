93709fa551862e7e0b07aa82c1eee88390d88df5
yaiza42-cmd <mcuentapersonal42@gmail.com>
2026-03-28 10:09:30 -0400

    Update twrp_austin.mk

--- twrp_austin.mk
+++ twrp_austin.mk
@@ -1,7 +1,12 @@
+# Heredar de la base de AOSP para TWRP
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
+# Identificadores del dispositivo (Muy importantes para el constructor)
 PRODUCT_DEVICE := austin
-PRODUCT_NAME := omni_austin
+PRODUCT_NAME := twrp_austin
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := Moto G 5G 2022
 PRODUCT_MANUFACTURER := motorola
+
+# Configuración de la compilación
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
