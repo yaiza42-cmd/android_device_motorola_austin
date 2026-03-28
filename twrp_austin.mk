7e9eeb3aa3842de1218b07193ea4f16ff40a1041
yaiza42-cmd <mcuentapersonal42@gmail.com>
2026-03-28 09:59:17 -0400

    Add files via upload

---
+++ twrp_austin.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+PRODUCT_DEVICE := austin
+PRODUCT_NAME := omni_austin
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := Moto G 5G 2022
+PRODUCT_MANUFACTURER := motorola
