924d54b0d8d10264ccae84a09d95858748a57d9c
yaiza42-cmd <mcuentapersonal42@gmail.com>
2026-03-28 12:04:54 -0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,10 +20,10 @@ BOARD_MKBOOTIMG_ARGS := --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PA
 # Particiones Dinámicas y A/B
 AB_OTA_UPDATER := true
 
-# Definición del grupo y tamaño (Basado en tu super.img de 8.5GB)
+# Basado en tu volcado de 7.8GB con margen para el contenedor super
 BOARD_SUPER_PARTITION_SIZE := 9126805504
 BOARD_SUPER_PARTITION_GROUPS := motorola_dynamic_partitions
-BOARD_MOTOROLA_DYNAMIC_PARTITIONS_SIZE := 9122611200
+BOARD_MOTOROLA_DYNAMIC_PARTITIONS_SIZE := 8589934592
 BOARD_MOTOROLA_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext product vendor
 
 # Arquitectura y Apps
