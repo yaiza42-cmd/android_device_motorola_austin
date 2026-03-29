1ab3b6be9689e08efb0354f49918f234575d1f11
yaiza42-cmd <mcuentapersonal42@gmail.com>
2026-03-29 09:45:55 -0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,6 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/OrangeFox.mk
+    $(LOCAL_DIR)/twrp_austin.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_austin-eng
+    twrp_austin-eng \
+    twrp_austin-userdebug
