80259fdbb181402822f7de987cfcdfeded746c52
yaiza42-cmd <mcuentapersonal42@gmail.com>
2026-03-31 05:44:26 -0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,27 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 DEVICE_PATH := device/motorola/austin
+
+# Bootloader
+BOARD_VENDOR := motorola
+TARGET_SOC := mt6853
+TARGET_BOOTLOADER_BOARD_NAME := mt6853
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
 
 # --- ARQUITECTURA Y PLATAFORMA ---
 TARGET_ARCH := arm64
