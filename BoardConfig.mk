fb6fd411bd9ef49aa341b491dfcfb46e50e4bee6
yaiza42-cmd <mcuentapersonal42@gmail.com>
2026-04-02 06:36:34 -0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,6 +60,7 @@ TARGET_BOARD_PLATFORM_GPU := mali-g57mc2
 
 # A/B
 AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += boot system system_ext vendor product vbmeta dtbo
 TARGET_NO_RECOVERY := true
 BOARD_USES_RECOVERY_AS_BOOT := true
 
