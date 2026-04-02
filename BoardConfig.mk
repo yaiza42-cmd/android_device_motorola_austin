147aba976b27911fd14edad0fe1cb8cdcb292902
yaiza42-cmd <mcuentapersonal42@gmail.com>
2026-04-02 07:31:51 -0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,6 +64,9 @@ AB_OTA_PARTITIONS += boot system system_ext vendor product vbmeta dtbo
 TARGET_NO_RECOVERY := true
 BOARD_USES_RECOVERY_AS_BOOT := true
 
+# Fastbootd
+TW_INCLUDE_FASTBOOTD := true
+
 # Kernel / mkbootimg args
 # Kernel / DTB
 BOARD_KERNEL_IMAGE_NAME := kernel
